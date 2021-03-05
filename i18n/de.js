38cf8e837f69e5109ab06a8ab47f580af397ca60
Michael Rittmeister <mail@schlaubi.me>
2021-03-05 23:38:09 +0100

    New translations en.js (German)

--- i18n/de.js
+++ i18n/de.js
@@ -1,15 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default (context) => {
-  return new Promise(function (resolve) {
-    resolve({
-      java_is_old: 'Java 8 ist heute {specifier} alt.',
-      loading: 'sehr', // sehr alt
-      and: 'und',
-      time: {
-        years: 'ein Jahr | {n} Jahre',
-        months: 'einen Monat | {n} Monate',
-        days: 'einen Tag | {n} Tage'
-      }
-    })
-  })
-}
+export default context => {
+    return new Promise(function (resolve) {
+        resolve({
+            java_is_old: 'Java 8 is {specifier} old today.',
+            loading: 'very',
+            // very old
+            and: 'and',
+            time: {
+                years: 'one year | {n} years',
+                months: 'one month | {n} months',
+                days: 'one day | {n} days'
+            }
+        });
+    });
+};
