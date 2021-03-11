ef57a665e94fe495e6004fd246a16a5e3d56121f
Michael Rittmeister <michael@rittmeister.in>
2021-03-11 14:36:33 +0100

    Fix source file

--- i18n/en/strings.js
+++ i18n/en/strings.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default context => {
-    return new Promise(function (resolve) {
-        resolve({
-            java_is_old: 'Java 8 is {specifier} old today.',
-            loading: 'very',
-            // very old
-            and: 'and',
-            time: {
-                years: 'one year | {n} years',
-                months: 'one month | {n} months',
-                days: 'one day | {n} days'
-            }
-        });
-    });
-};
+export default (context) => {
+  return new Promise(function (resolve) {
+    resolve({
+      java_is_old: 'Java 8 is {specifier} old today.',
+      loading: 'very',
+      // very old
+      and: 'and',
+      time: {
+        years: 'one year | {n} years',
+        months: 'one month | {n} months',
+        days: 'one day | {n} days'
+      }
+    })
+  })
+}
