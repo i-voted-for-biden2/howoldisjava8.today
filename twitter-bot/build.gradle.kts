4def60bd218df5995a5b23bfb03155288c17fee4
NyCodeGHG <nycode@protonmail.com>
2021-03-11 19:38:45 +0100

    Add twitter bot

---
+++ twitter-bot/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  kotlin("jvm") version "1.4.31"
+  kotlin("plugin.serialization") version "1.4.31"
+}
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  implementation("io.github.cdimascio", "dotenv-kotlin", "6.2.2")
+  implementation("io.ktor", "ktor-client-okhttp", "1.5.2")
+  implementation("io.ktor", "ktor-client-serialization", "1.5.2")
+  implementation("com.github.redouane59.twitter", "twittered", "1.20")
+  implementation("org.jetbrains.kotlinx", "kotlinx-datetime", "0.1.1")
+
+  implementation("org.slf4j", "slf4j-simple", "1.7.30")
+
+  implementation("dev.inmo", "krontab", "0.5.0")
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "1.8"
+      freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+  }
+}
