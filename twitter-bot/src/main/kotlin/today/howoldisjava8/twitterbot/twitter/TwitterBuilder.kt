9323e8d168904f1bd3bcac4555f3f74f4432209a
NyCodeGHG <nycode@protonmail.com>
2021-03-11 19:38:45 +0100

    Add twitter bot

---
+++ twitter-bot/src/main/kotlin/today/howoldisjava8/twitterbot/twitter/TwitterBuilder.kt
@@ -0,0 +1,9 @@
+package today.howoldisjava8.twitterbot.twitter
+
+import com.github.redouane59.twitter.signature.TwitterCredentials
+
+fun credentials(builder: TwitterCredentials.() -> Unit): TwitterCredentials {
+  val credentials = TwitterCredentials()
+  credentials.apply(builder)
+  return credentials
+}
