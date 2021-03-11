e04102a041214e0c5b294af5e6b8607c7fcc098a
Michael Rittmeister <mail@schlaubi.me>
2021-03-11 14:31:43 +0100

    New translations strings.js (German)

---
+++ i18n/de/strings.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default context => {
+    return new Promise(function (resolve) {
+        resolve({
+            java_is_old: 'Java is heute {specifier} alt.',
+            loading: 'sehr',
+            // very old
+            and: 'und',
+            time: {
+                years: 'ein Jahr | {n} Jahre',
+                months: 'ein Monat | {n} Monate',
+                days: 'ein Tag | {n} Tage'
+            }
+        });
+    });
+};
