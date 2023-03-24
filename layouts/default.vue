c1c78c124282aaffbc5874479c5a10a7c5541249
Michael Rittmeister <michael@rittmeister.in>
2023-03-24 14:56:58 +0100

    Remove affiliation with community operated mastodon bot

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-  <v-fragment>
-    <a rel="me" href="https://botsin.space/@HowOldIsJava8" class="hidden">Mastodon</a>
-    <div class="flex h-screen items-center justify-center">
-      <Nuxt />
+  <div>
+    <div class="flex flex-col h-screen">
+      <Nuxt class="flex-grow" />
     </div>
-  </v-fragment>
+  </div>
 </template>
